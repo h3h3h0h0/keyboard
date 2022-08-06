d8ede4216f2fe5f7e4595a8685cd9731b14ed853
h3h3h0h0 <deboss.wt@gmail.com>
2022-08-06 14:41:52 -0400

    wrote some of the main module and made the keypress identifier module

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from machine import I2C
+from keyinterface import KeyInterface
 
-#some reference values
+#layout
 deviceNum = 4 #4 8-channel multiplexers each driving 32 keys (4 addresses per pin)
 channelNum = 8 #8 channel multiplexers
 keysPerChannel = 4 #4 individual key addresses per channel
 
+numKeys = 104 #standard full-size keyboard
+
+#address lookups
+channelList = []
+keyList = []
+
+#resolution of sensors
+resolution = 0
+
 #create I2C in fast mode
-#scans for multiplexers controlling the key sensors
 i2c = I2C(freq=400000)
 
-#main loop (the while condition will be changed later)
-while True:
-    #read each multiplexer
-    for device in deviceList:
+#initialize the actual interface
+KI = KeyInterface(i2c, deviceNum, channelNum, keysPerChannel, channelList, keyList, numKeys)
 
